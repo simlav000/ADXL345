05d544d72d67247f6af5552c5e82fc982afcbe75
simlav000 <simon.lavoie@mail.mcgill.ca>
2026-03-12 14:02:13 -0400

    [settings] Back to MikroE chip

--- menu.py
+++ menu.py
@@ -38,7 +38,7 @@ def confirm_settings(odr, g_range):
 
 def main():
     BUS_NUM = 1
-    DEVICE_ADDR = 0x53  # typical ADXL345 address
+    DEVICE_ADDR = 0x1D
 
     default_odr = OutputDataRate.ODR_100
     default_range = Range.RANGE_FULL
